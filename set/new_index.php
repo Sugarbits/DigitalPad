421813a824fe666ab3fc77e913398270d9cab853
Sugarbits <b1030536039@gmail.com>
2017-12-10 03:59:49 +0800

    buck

--- set/new_index.php
+++ set/new_index.php
@@ -5,7 +5,6 @@
 <script src="../plugin/jqGrid/src/i18n/grid.locale-en.js" type="text/javascript"></script>
 <script src="../plugin/jqGrid/js/jquery.jqGrid.min.js" type="text/javascript"></script>
 <script src="../plugin/jquery-ui/jquery-ui.min.js" type="text/javascript"></script>
-<<<<<<< HEAD
 <style>
 .clearfix {
   overflow: auto;
@@ -23,8 +22,6 @@
 }
 .div_b iframe {display: block; width: 100%; height: 100%; border: none;}
 </style>
-=======
->>>>>>> 17ec98396429877fbaaa8969ccf501cca2b48454
 </head>
 
 <?php
@@ -86,7 +83,6 @@ fclose($myfile);
 <button id="word_fix">修改</button>
 	</div>
   <div id="tabs-2">
-<<<<<<< HEAD
   <div class='clearfix'>
 	<div class='div_a'>
 		<iframe  style='overflow: visible; display: block; position: relative; visibility: visible; z-index: 0;' scrolling="no" src="../new_picture_tb.php"  frameBorder="0">
@@ -100,18 +96,6 @@ fclose($myfile);
 	</div>
  </div>
 </div>
-=======
-	 <?php echo $tb;?>
-		<div class="form-wrap">
-		<h3>Ajax Image Uploader</h3>
-			<form action="../plugin/ajax-image-upload-master/process.php" method="post" enctype="multipart/form-data" id="upload_form">
-			<input name="__files[]" type="file" multiple />
-			<input name="__submit__" type="submit" value="Upload"/>
-			</form>
-    <div id="output"><!-- error or success results --></div>
-</div>
-	</div>
->>>>>>> 17ec98396429877fbaaa8969ccf501cca2b48454
   <div id="tabs-3">
 	 <?php echo $tb;?>
 		<div class="form-wrap">
@@ -271,79 +255,4 @@ var result_output 		= '#output'; //ID of an element for response output
 var my_form_id 			= '#upload_form'; //ID of an element for response output
 var total_files_allowed 	= 3; //Number files allowed to upload
 
-
-
-//on form submit
-$(my_form_id).on( "submit", function(event) { 
-	event.preventDefault();
-	var proceed = true; //set proceed flag
-	var error = [];	//errors
-	var total_files_size = 0;
-	
-	if(!window.File && window.FileReader && window.FileList && window.Blob){ //if browser doesn't supports File API
-		error.push("Your browser does not support new File API! Please upgrade."); //push error text
-	}else{
-		var total_selected_files = this.elements['__files[]'].files.length; //number of files
-		
-		//limit number of files allowed
-		if(total_selected_files > total_files_allowed){
-			error.push( "You have selected "+total_selected_files+" file(s), " + total_files_allowed +" is maximum!"); //push error text
-			proceed = false; //set proceed flag to false
-		}
-		 //iterate files in file input field
-		$(this.elements['__files[]'].files).each(function(i, ifile){
-			if(ifile.value !== ""){ //continue only if file(s) are selected
-				if(allowed_file_types.indexOf(ifile.type) === -1){ //check unsupported file
-					error.push( "<b>"+ ifile.name + "</b> is unsupported file type!"); //push error text
-					proceed = false; //set proceed flag to false
-				}
-				total_files_size = total_files_size + ifile.size; //add file size to total size
-			}
-		});
-		
-		//if total file size is greater than max file size
-		if(total_files_size > max_file_size){ 
-			error.push( "You have "+total_selected_files+" file(s) with total size "+total_files_size+", Allowed size is " + max_file_size +", Try smaller file!"); //push error text
-			proceed = false; //set proceed flag to false
-		}
-		
-		var submit_btn  = $(this).find("input[type=submit]"); //form submit button	
-		
-		//if everything looks good, proceed with jQuery Ajax
-		if(proceed){
-			submit_btn.val("Please Wait...").prop( "disabled", true); //disable submit button
-			var form_data = new FormData(this); //Creates new FormData object
-			var post_url = $(this).attr("action"); //get action URL of form
-			//console.log(post_url);
-			
-			//jQuery Ajax to Post form data
-			$.ajax({
-				url : post_url,
-				type: "POST",
-				data : form_data,
-				contentType: false,
-				cache: false,
-				processData:false,
-				mimeType:"multipart/form-data"
-			}).done(function(res){ //
-				$(my_form_id)[0].reset(); //reset form
-				$(result_output).html(res); //output response from server
-				submit_btn.val("Upload").prop( "disabled", false); //enable submit button once ajax is done
-				//更新list
-				var newdata = JSON.parse(ajax_text_jsonread());
-				//console.log(newdata);
-				$("#list4").clearGridData();
-				for(var i=0;i<=newdata.length;i++)
-					$("#list4").jqGrid('addRowData',i+1,newdata[i]);
-				//更新list_END
-			});
-		}
-	}
-	
-	$(result_output).html(""); //reset output 
-	$(error).each(function(i){ //output any error to output element
-		$(result_output).append('<div class="error">'+error[i]+"</div>");
-	});
-		
-});
 </script>
